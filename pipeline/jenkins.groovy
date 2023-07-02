089e51017e5bda7d27d31708dc4b313b72762642
spolish <88062507+spolish@users.noreply.github.com>
2023-07-02 00:39:21 +0000

    jenkins sample

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,5 +1,52 @@
 pipeline {
     agent any
+
+    parameters {
+
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+
+    }
+
+    stages {
+        stage('CI') {
+            steps {
+                checkout scm
+
+                sh 'make test'
+                
+                withDockerRegistry([credentialsId: 'docker-registry-credentials', url: 'https://ghcr.io']) {
+                    sh 'make image push'
+                }
+            }
+        }
+
+        stage('CD') {
+            dependencies {
+                stage('CI')
+            }
+            steps {
+                checkout scm
+
+                sh 'echo "VERSION=$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)" > version.env'
+                sh 'echo "TARGETARCH=amd64" >> version.env'
+                
+                sh 'docker run --rm -v $PWD:/workdir mikefarah/yq yq -i \'.image.tag=env.VERSION | .image.arch=env.TARGETARCH\' helm/values.yaml'
+
+                sh '''
+                git config user.name 'jenkins'
+                git config user.email 'jenkins@example.com'
+                git add .
+                git commit -m "Update version $VERSION"
+                git push
+                '''
+            }
+        }
+    }
+}
+
+
+pipeline {
+    agent any
     parameters {
 
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
