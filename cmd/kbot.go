efc400c07ca6bb1651e95dc1531a7333e5a7373f
spolish <88062507+spolish@users.noreply.github.com>
2023-07-04 22:01:55 +0000

    tesr

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -14,8 +14,8 @@ import (
 )
 
 // TeleToken bot
-var TeleToken = 6369096639:AAFC85t4AHcljTwSRvL6B_w8DUUKKSj-kNQ
-//var TeleToken = os.Getenv("TELE_TOKEN")
+///var TeleToken = 
+var TeleToken = os.Getenv("TELE_TOKEN")
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
